750dd9e982f122574a2b0851eb035db30d2da625
Sarthakls9 <109941152+Sarthakls9@users.noreply.github.com>
2023-08-03 11:02:25 +0530

    Add files via upload

---
+++ 9921103001 OSSL Lab week 1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, pro;
+    printf("Enter First Number: ");
+    scanf("%d", &a);
+    printf("Enter Second Number: ");
+    scanf("%d", &b);
+    pro = a * b;
+    printf("The product of the Numbers is: %d", pro);
+    return 0;
+}
